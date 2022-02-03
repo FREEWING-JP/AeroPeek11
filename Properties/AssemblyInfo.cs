406370244d57c6d729a35c10e1f295feb38a625d
Noah Bartley <98945112+noahlbartley@users.noreply.github.com>
2022-02-03 15:30:30 -0600

    Add the correct properties files to the folder.
    Added the project's properties files to the properties folder in the repository.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AeroPeek11")]
+[assembly: AssemblyDescription("Bring back the Aero Peek feature in Windows 11.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Noah Bartley")]
+[assembly: AssemblyProduct("AeroPeek11")]
+[assembly: AssemblyCopyright("Copyright © Noah Bartley")]
+[assembly: AssemblyTrademark("https://github.com/noahlbartley")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e23ffa3d-b462-4d9c-a2e1-47009622e549")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
